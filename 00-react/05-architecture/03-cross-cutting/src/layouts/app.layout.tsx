4e2d8a40811e29b6e60ea30a7799be558ee18cb3
Braulio Diez Botella <braulio.diez@gmail.com>
2022-01-28 21:07:03 +0100

    cross cutting completed

---
+++ 00-react/05-architecture/03-cross-cutting/src/layouts/app.layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ProfileContext } from "@/core/profile";
+
+export const AppLayout: React.FC = ({ children }) => {
+  const { userName } = React.useContext(ProfileContext);
+
+  return (
+    <div className="layout-app-container">
+      <div className="layout-app-header">{userName}</div>
+      {children}
+    </div>
+  );
+};
